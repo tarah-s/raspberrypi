809a92e0ed206d015e7348fc459b40bd34746c31
tarah soljan <Tcharmian@gmail.com>
2016-02-20 19:39:12 +1100

    sound test

--- random-sound/play-sound.sh
+++ random-sound/play-sound.sh
@@ -2,6 +2,11 @@
 # play-sound.sh plays a random sound file on run
 # sounds file examples from https://www.reddit.com/r/DotA2/comments/2ulnjy/nba_jam_announcer_megakill_pack_working_with/
 
+
+# you can install mpg321 to play mp3 files with command control of the volume
+# Omxplayer seems to cut short mp3 files, prefer to use MPG321 over Omxplayer
+# sudo apt-get -y install mpg321
+
 Sounds="/PATH/TO/FILE/announcer_01.mp3
 /PATH/TO/FILE/announcer_02.mp3
 /PATH/TO/FILE/announcer_03.mp3
@@ -22,6 +27,8 @@ num_sounds=${#sounds[*]}
 afplay ${sounds[$((RANDOM%num_sounds))]} 
 
 
+# Default Omxplayer
+
 # rpi sound through auto ( hdmi )
 # omxplayer ${sounds[$((RANDOM%num_sounds))]}
 
@@ -30,3 +37,9 @@ afplay ${sounds[$((RANDOM%num_sounds))]}
 
 # rpi forces sound through audio jack
 # omxplayer -o local ${sounds[$((RANDOM%num_sounds))]}
+
+
+# Using MPG321
+
+# MPG321 with sound percentage control
+# mpg321 -g 100 ${sounds[$((RANDOM%num_sounds))]}
