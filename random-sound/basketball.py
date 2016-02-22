6d6b54c18d8fe6e34295be700c4a70cd634eb01c
tarah soljan <Tcharmian@gmail.com>
2016-02-23 07:57:17 +1100

    button count

--- random-sound/basketball.py
+++ random-sound/basketball.py
@@ -11,8 +11,6 @@ butPin = 17 # Broadcom pin 17 (P1 pin 11)
 # Pin Setup:
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme 
 GPIO.setup(butPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
- 
-scoreCount = 0
 
 print("Here we go! Press CTRL+C to exit")
 firebase = firebase.FirebaseApplication('https://', None)
